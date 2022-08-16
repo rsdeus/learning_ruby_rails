d59cb06807e24b48ab04c96beb5b7d6afb40bd01
Renato da Silva de Deus <rsdeus@gmail.com>
2022-08-16 10:44:28 -0300

    exercicies about include, inherited, extend and prepend

--- validation.rb
+++ validation.rb
@@ -1,20 +1,49 @@
-module Validation
+module Container
 
-  attr_accessor :foo
+  attr_accessor :container
+
+  module ArrangeClothes
+
+    def container
+      @container ||= []
+    end
+  
+    def add_clothes(piece=nil)
+      @container.push(piece) unless piece.nil?
+    end
+  
+    def check_clothes(piece)
+      @container.include? piece
+    end
+  
+    def get_clothes(piece)
+      if @container.include?(piece)
+        @container.at(@container.find_index(piece))
+        @container.delete_at(@container.find_index(piece))
+      end
+    end
+  
+  end
+
+  def self.included(container)
+    container.extend(ArrangeClothes)
+  end
 
   def empty?
-    return true if foo.nil?
-    foo.empty?
+    return true if container.nil?
+    container.empty?
   end
 
 end
 
-class Gaveta
 
-  include Validation
 
-  def initialize(foo=nil)
-    @foo = foo
+class Wardrobe
+
+  include Container
+
+  def initialize(container=nil)
+    @container = container
   end
 
 end
